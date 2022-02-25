684f14001a7f13c9c179e2d76114118462e853e2
ann <asog.jrti@gmail.com>
2022-02-25 17:54:11 +0100

    starts using bootstrap

--- students/ann/cocktails-app/src/pages/home/index.jsx
+++ students/ann/cocktails-app/src/pages/home/index.jsx
@@ -14,7 +14,7 @@ function Home() {
                         alt="First slide"
                     />
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
+                        <h3>Learn to prepare your favourite cocktail</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
@@ -26,22 +26,11 @@ function Home() {
                     />
 
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
+                        <h3>Ordinary drinks and alcoholic drinks</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={cocktail1}
-                        alt="Third slide"
-                    />
 
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
             </Carousel>
         </React.Fragment>
 
